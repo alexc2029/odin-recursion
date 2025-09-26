111a5f3d0b4e4a2253c61fd7f82f777bc0a9ffdc
alexc2029 <alex2akec@gmail.com>
2025-09-26 10:03:01 +0300

    Finish mergesort

---
+++ mergesort.js
@@ -0,0 +1,31 @@
+function merge(arr1, arr2) {
+	let i = 0,
+		j = 0;
+	mergedArr = [];
+	while (i < arr1.length && j < arr2.length) {
+		if (arr1[i] <= arr2[j]) {
+			mergedArr.push(arr1[i]);
+			i++;
+		} else {
+			mergedArr.push(arr2[j]);
+			j++;
+		}
+	}
+	while (i < arr1.length) {
+		mergedArr.push(arr1[i]);
+		i++;
+	}
+	while (j < arr2.length) {
+		mergedArr.push(arr2[j]);
+		j++;
+	}
+	return mergedArr;
+}
+
+function mergeSort(arr) {
+	if (arr.length == 1) return arr;
+	const mid = Math.ceil(arr.length / 2);
+	return merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(-mid)));
+}
+
+console.log(mergeSort([105, 79, 100, 110]));
